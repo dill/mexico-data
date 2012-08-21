0a7f86cc742bf8f1f195673576f9de370c2733ec
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-20 22:23:15 -0400

    update to paper plots

--- paperplots/fit-depth.R
+++ paperplots/fit-depth.R
@@ -14,35 +14,40 @@ library(maps)
 set.seed(11123)
 
 library(maptools)
-survey.area<-readShapeSpatial("data/Study_ar")
+survey.area<-readShapeSpatial("../data/Study_ar")
 survey.area<-data.frame(survey.area@polygons[[1]]@Polygons[[1]]@coords)
 names(survey.area)<-c("longitude","latitude")
-load("data/dolphins.RData")
+load("../data/dolphins.RData")
 lon0 <- -88.31951
 lat0 <- 27.01594
 library(dsm)
 sa.tmp <- latlong2km(survey.area$longitude, survey.area$latitude, 
 lon0=lon0, lat0=lat0)
-survey.area <- data.frame(x=1000*sa.tmp$km.e, y=1000*sa.tmp$km.n)
+#survey.area <- data.frame(x=1000*sa.tmp$km.e, y=1000*sa.tmp$km.n)
+survey.area <- data.frame(x=sa.tmp$km.e, y=sa.tmp$km.n)
 rm(sa.tmp)
 
 suppressPackageStartupMessages(library(Distance))
 hn.model<-ds(distdata,max(distdata$distance),monotonicity="strict")
 
-off.set <- fitted(hn.model$ddf)[1]*preddata$width*preddata$height
+#off.set <- preddata$width*preddata$height
+off.set <- 444*1000^2 
 mod2<-dsm.fit(hn.model$ddf, response="indiv", formula=~s(depth), obsdata=obsdata,segdata=segdata)
-mod2.pred<-dsm.predict(mod2, preddata,off=off.set)
+mod2.pred<-dsm.predict(mod2, preddata, off.set)
 cat("depth abundance=",sum(mod2.pred),"\n")
 
 mod2.xy<-dsm.fit(hn.model$ddf, response="indiv", formula=~s(x,y)+s(depth), obsdata=obsdata,segdata=segdata)
-mod2.xy.pred<-dsm.predict(mod2.xy, preddata,off=off.set)
+mod2.xy.pred<-dsm.predict(mod2.xy, preddata, off.set)
 cat("depth+x-y abundance=",sum(mod2.xy.pred),"\n")
 
-
+# plot the check
+pdf("dsm-check.pdf",width=7,height=7)
+dsm.check(mod2.xy)
+dev.off()
 
 # plot stuff
 
-plot.breaks <- c(0,10,50,100,200,500,1000,2000,3000,5500)
+plot.breaks <- c(0,10,50,100,200,500,1000,2000,3000)#,5000,7000,7500)
 
 plot.lims<-c(0,max(mod2.pred,mod2.xy.pred))
 #plot.lims<-c(0,3000)
@@ -56,8 +61,15 @@ grad.obj <- scale_fill_gradientn(colours=heat_hcl(length(plot.breaks)-1),
 
 ## @knitr mod2-preds
 pp<-cbind(preddata,mod2.pred)
+
+# to get northings and eastings
+pp$x<-pp$x/1000
+pp$y<-pp$y/1000
+pp$width <- pp$width/1000
+pp$height <- pp$height/1000
+
 p<-ggplot(pp)+gg.opts
-p <- p + labs(fill="Abundance",x="Metres from centre point",y="Metres from centre point")
+p <- p + labs(fill="Abundance",x="Easting",y="Northing")
 p<-p+geom_tile(aes(x=x,y=y,fill=mod2.pred,width=width,height=height))
 p <- p + coord_equal()
 p <- p + grad.obj 
@@ -65,17 +77,22 @@ p<-p+geom_path(aes(x=x, y=y),data=survey.area)
 print(p)
 ggsave("fit-depth.pdf", width=9,height=5)
 
-quartz()
+#quartz()
 pdf("fit-depth-gam.pdf", width=5,height=5)
 plot(mod2$result,select=1)
 dev.off()
 
-quartz()
+#quartz()
 
 ## @knitr mod2-preds
 pp<-cbind(preddata,mod2.xy.pred)
+# to get northings and eastings
+pp$x<-pp$x/1000
+pp$y<-pp$y/1000
+pp$width <- pp$width/1000
+pp$height <- pp$height/1000
 p<-ggplot(pp)+gg.opts
-p <- p + labs(fill="Abundance",x="Metres from centre point",y="Metres from centre point")
+p <- p + labs(fill="Abundance",x="Easting",y="Northing")
 p<-p+geom_tile(aes(x=x,y=y,fill=mod2.xy.pred,width=width,height=height))
 p <- p + coord_equal()
 p <- p + grad.obj 

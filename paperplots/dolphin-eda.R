90b1b0b127b2cc4ca4d7ce262e906b975ddf2823
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-20 23:01:39 -0400

    more plot regeneration (using Ne Ea)

--- paperplots/dolphin-eda.R
+++ paperplots/dolphin-eda.R
@@ -11,28 +11,38 @@ library(maps)
 # make the results reproducable
 set.seed(11123)
 library(maptools)
-survey.area<-readShapeSpatial("data/Study_ar")
+survey.area<-readShapeSpatial("../data/Study_ar")
 survey.area<-data.frame(survey.area@polygons[[1]]@Polygons[[1]]@coords)
 names(survey.area)<-c("longitude","latitude")
-load("data/dolphins.RData")
+load("../data/dolphins.RData")
 # centroid
 lon0 <- -88.31951
 lat0 <- 27.01594
 library(dsm)
 sa.tmp <- latlong2km(survey.area$longitude, survey.area$latitude, 
 lon0=lon0, lat0=lat0)
-survey.area <- data.frame(x=1000*sa.tmp$km.e, y=1000*sa.tmp$km.n)
+#survey.area <- data.frame(x=1000*sa.tmp$km.e, y=1000*sa.tmp$km.n)
+survey.area <- data.frame(x=sa.tmp$km.e, y=sa.tmp$km.n)
 rm(sa.tmp)
 
 
 
 ### actual plotting code here
 
+preddata$x <- preddata$x/1000
+preddata$y <- preddata$y/1000
+preddata$width <- preddata$width/1000
+preddata$height <- preddata$height/1000
+segdata$x <- segdata$x/1000
+segdata$y <- segdata$y/1000
+distdata$x <- distdata$x/1000
+distdata$y <- distdata$y/1000
 
 p <- ggplot(preddata)
 p <- p + gg.opts
 p <- p + coord_equal()
-p <- p + labs(fill="Depth (m)",x="Metres from centre point",y="Metres from centre point",size="Group size")
+#p <- p + labs(fill="Depth (m)",x="Metres from centre point",y="Metres from centre point",size="Group size")
+p <- p + labs(fill="Depth (m)",x="Easting",y="Northing",size="Group size")
 p <- p + geom_tile(aes(x=x,y=y,fill=depth, width = width, height = height))
 p <- p + geom_line(aes(x, y, group=Transect.Label),data=segdata)
 p <- p + geom_point(aes(x, y, size=size), data=distdata, colour="blue",alpha=I(0.7))

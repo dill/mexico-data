b06952b7359b5ecfb430fb7d7424c3d7dfcda91a
David Lawrence Miller <dave@ninepointeightone.net>
2012-12-21 17:54:35 +0000

    paperish?

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 R -e 'library(knitr);library(markdown);knit("mexico-analysis.Rmd"); markdownToHTML("mexico-analysis.md","mexico-analysis.html")'
+
+pandoc -s -i mexico-analysis.md -o mexico-analysis.tex -t latex -S

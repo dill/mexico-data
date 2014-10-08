90c56c696cb979df105da9f26dc73680597dbf3f
David Lawrence Miller <github@ninepointeightone.net>
2014-10-08 18:22:08 -0400

    updated mexico analysis to work with dsm 2.2.5
    + new Tweedie/negbin info
    
    -> distancesampling.org

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@
 #pandoc -s -i mexico-analysis.md -o mexico-analysis.tex -t latex -S
 
 
+# make md to pdf
+#R -e 'library(knitr);knit("mexico-analysis.Rmd")'
+#pandoc -s -i mexico-analysis.md -o mexico-analysis.tex -t latex -S -N
 
-R -e 'library(knitr);knit("mexico-analysis.Rmd")'
-
-pandoc -s -i mexico-analysis.md -o mexico-analysis.tex -t latex -S -N
+R -e 'library(rmarkdown);render("mexico-analysis.Rmd",html_document())'

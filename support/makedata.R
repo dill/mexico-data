2ef83b294a2baf36af93128655b645acb74a7eec
David Lawrence Miller <github@ninepointeightone.net>
2015-06-02 16:58:32 -0400

    first run through using corrected areas

--- support/makedata.R
+++ support/makedata.R
@@ -1,21 +1,25 @@
-### RUN THIS FROM THE TOP LEVEL!!!
-
 library(dsm)
+library(rgdal)
+library(maptools)
+library(plyr)
 
-# three csv files that are copy and pasted from Distance data viewer
-# mexico-observation.csv
-# mexico-segment.csv
-# mexico-transect.csv -- pretty sure we can ignore this one
+options(stringsAsFactors=FALSE)
 
-mex.obs<-read.csv("data/csv/mexico-observation.csv",header=FALSE)
-mex.seg<-read.csv("data/csv/mexico-segment.csv",header=FALSE)
-#mex.tran<-read.csv("data/csv/mexico-transect.csv",header=FALSE)
-mex.pred <- read.table("data/csv/mexico-pred.txt",skip=5)
 
+obs<-read.csv("../data/csv/mexico-observation.csv",header=FALSE)
+seg<-read.csv("../data/csv/mexico-segment.csv",header=FALSE)
+#tran<-read.csv("../data/csv/mexico-transect.csv",header=FALSE)
+pred <- readShapeSpatial("../data/DProject/Dolphins.dat/Predict")
+pred.d <- read.table("../data/csv/mexico-pred.txt",skip=5)
+survey.area <- readShapeSpatial("../data/DProject/Dolphins.dat/Study_Ar")
+
+# proj 4 string
+# using http://spatialreference.org/ref/esri/north-america-lambert-conformal-conic/
+lcc_proj4 <- CRS("+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
 
 # centroid of the survey area
-lon0 <- -88.31951
-lat0 <- 27.01594
+#lon0 <- -88.31951
+#lat0 <- 27.01594
 
 
 # create obsdata
@@ -25,16 +29,16 @@ lat0 <- 27.01594
 # * ``distance`` - perpendicular/radial distance to observation
 
 # get rid of the column titles put there by Distance
-obs.tmp <- mex.obs[6:nrow(mex.obs),]
+obs <- obs[6:nrow(obs),]
 
 
 
 obsdata <- data.frame(
-                      object=1:nrow(obs.tmp),
-                      Sample.Label=as.character(obs.tmp[,13]),
-                      size=as.numeric(as.character(obs.tmp[,21])),
-                      distance=as.numeric(as.character(obs.tmp[,19])),
-                      Effort=1000*as.numeric(as.character(obs.tmp[,14]))
+                      object=1:nrow(obs),
+                      Sample.Label=obs[,13],
+                      size=as.numeric(obs[,21]),
+                      distance=as.numeric(obs[,19]),
+                      Effort=1000*as.numeric(obs[,14])
                      )
 obsdata$size[is.na(obsdata$size)] <- 0
 obsdata$distance[is.na(obsdata$distance)] <- 0
@@ -48,64 +52,112 @@ obsdata$distance[is.na(obsdata$distance)] <- 0
 # * ``esw`` - effective strip width... ?
 
 # top 5 rows are the column titles from Distance
-seg.tmp <- mex.seg[6:nrow(mex.seg),]
+seg <- seg[6:nrow(seg),]
 
-# use lat and long for now
-segdata <- data.frame(
-                      latitude=as.numeric(as.character(seg.tmp[,15])),
-                      longitude=as.numeric(as.character(seg.tmp[,16])),
-                      Effort=1000*as.numeric(as.character(seg.tmp[,14])),
-                      Transect.Label=as.character(seg.tmp[,9]),
-                      Sample.Label=as.character(seg.tmp[,13]),
-                      depth=as.double(as.numeric(as.character(seg.tmp[,17])))
+# make an sp object to project
+segsp <- SpatialPoints(cbind(as.numeric(seg[,16]),
+                             as.numeric(seg[,15])))
+
+# give the sp object a projection
+proj4string(segsp) <-CRS("+proj=longlat +datum=WGS84")
+# re-project
+segsp.t <- spTransform(segsp,CRSobj=lcc_proj4)
+
+# build segment data
+segdata <- data.frame(longitude      = as.numeric(seg[,16]),
+                      latitude       = as.numeric(seg[,15]),
+                      x              = segsp.t@coords[,1],
+                      y              = segsp.t@coords[,2],
+                      Effort         = 1000*as.numeric(seg[,14]),
+                      Transect.Label = seg[,9],
+                      Sample.Label   = seg[,13],
+                      depth          = as.double(as.numeric(seg[,17]))
                      )
-
-# calculate the northings and eastings
-seg.tmp <- latlong2km(segdata$longitude, segdata$latitude, lon0=lon0, lat0=lat0)
-segdata <- cbind(segdata, x=1000*seg.tmp$km.e, y=1000*seg.tmp$km.n)
-rm(seg.tmp)
 
 ### distance data
 # create what we want for the mrds analysis
-distdata <- obsdata[obsdata$size>0,]
+distdata <- obsdata[obsdata$size>0, ]
 distdata$Sample.Label <- NULL
 distdata$detected <- rep(1,nrow(distdata))
-distdata$beaufort <- as.numeric(as.character(obs.tmp[,22][obsdata$size>0]))
 
+
+## get other data
+distdata$beaufort <- as.numeric(obs[,22][obsdata$size>0])
 # include the lat/long but just for plotting
-distdata$latitude <- as.numeric(as.character(obs.tmp[,15][obsdata$size>0]))
-distdata$longitude <- as.numeric(as.character(obs.tmp[,16][obsdata$size>0]))
+distdata$latitude <- as.numeric(obs[,15][obsdata$size>0])
+distdata$longitude <- as.numeric(obs[,16][obsdata$size>0])
+# make an sp object to project
+distsp <- SpatialPoints(cbind(distdata$longitude,
+                              distdata$latitude))
 
-ds.tmp <- latlong2km(distdata$longitude, distdata$latitude, lon0=lon0, lat0=lat0)
-distdata <- cbind(distdata, x=1000*ds.tmp$km.e, y=1000*ds.tmp$km.n)
-rm(ds.tmp)
+# give the sp object a projection
+proj4string(distsp) <-CRS("+proj=longlat +datum=WGS84")
+# re-project
+distsp.t <- spTransform(distsp,CRSobj=lcc_proj4)
+distdata$x <- distsp.t@coords[,1]
+distdata$y <- distsp.t@coords[,2]
 
 
 #### prediction data
-preddata<-data.frame(latitude=mex.pred[,7],
-                     longitude=mex.pred[,6],
-                     depth=as.double(mex.pred[,8]))
 
-pred.tmp <- latlong2km(preddata$longitude, preddata$latitude, lon0=lon0, lat0=lat0)
-preddata <- cbind(preddata, x=1000*pred.tmp$km.e, y=1000*pred.tmp$km.n)
+proj4string(pred) <- CRS("+proj=longlat +datum=WGS84")
 
-# find the width and height of each cell for plotting and finding the offset
-lr <- c(preddata$longitude-1/6, preddata$longitude+1/6)
-tb <- c(preddata$latitude-1/6, preddata$latitude+1/6)
+# grid spacing
+grid.eps <- 0.16666667/2
 
-lr.tmp <- latlong2km(lr, rep(preddata$latitude,2), lon0=lon0, lat0=lat0)
-tb.tmp <- latlong2km(rep(preddata$longitude,2), tb, lon0=lon0, lat0=lat0)
+# build some polygons
+ID <- 0
+pp <- alply(pred@coords, 1, function(x){
 
-preddata$width <- 1000*
-(lr.tmp$km.e[(length(preddata$latitude)+1):length(lr.tmp$km.e)]-
-lr.tmp$km.e[1:length(preddata$latitude)])
-preddata$height <- 1000*
-(tb.tmp$km.n[(length(preddata$latitude)+1):length(tb.tmp$km.n)]-
-tb.tmp$km.n[1:length(preddata$latitude)])
+  this.poly <- rbind(c(x[1]+grid.eps,x[2]+grid.eps),
+                     c(x[1]+grid.eps,x[2]-grid.eps),
+                     c(x[1]-grid.eps,x[2]-grid.eps),
+                     c(x[1]-grid.eps,x[2]+grid.eps),
+                     c(x[1]+grid.eps,x[2]+grid.eps))
 
-rm(lr, tb, lr.tmp, tb.tmp, pred.tmp)
+  ID <<- ID + 1
+  Polygons(list(Polygon(this.poly)),ID=as.character(ID))
+})
+
+# make polygon object, give it a projection
+pp <- SpatialPolygons(pp)
+proj4string(pp) <- CRS("+proj=longlat +datum=WGS84")
+
+
+# convert projection to lcc
+# for the squares
+pp.t <- spTransform(pp,CRSobj=lcc_proj4)
+# for points
+pred.t <- spTransform(pred,CRSobj=lcc_proj4)
+
+# test plot
+#par(mfrow=c(1,2))
+#plot(pred, pch=19,cex=0.2)
+#plot(pp,add=TRUE)
+#plot(pred.t, pch=19,cex=0.2)
+#plot(pp.t,add=TRUE)
+
+pred.polys <- pp.t
+
+preddata <- data.frame(latitude  = pred@coords[,1],
+                       longitude = pred@coords[,2],
+                       x         = pred.t@coords[,1],
+                       y         = pred.t@coords[,2],
+                       depth     = as.double(pred.d[,8]),
+                       area      = ldply(pp.t@polygons[[1]]@Polygons,
+                                   function(x) x@area)[[1]])
+
+
+#preddata$width <- 1000*
+#(lr.tmp$km.e[(length(preddata$latitude)+1):length(lr.tmp$km.e)]-
+#lr.tmp$km.e[1:length(preddata$latitude)])
+#preddata$height <- 1000*
+#(tb.tmp$km.n[(length(preddata$latitude)+1):length(tb.tmp$km.n)]-
+#tb.tmp$km.n[1:length(preddata$latitude)])
+
+#rm(lr, tb, lr.tmp, tb.tmp, pred.tmp)
 
 obsdata <- obsdata[obsdata$size>0,]
 
 # save everything to file
-save(segdata,obsdata,distdata,preddata,file="data/dolphins.RData")
+save(segdata, obsdata, distdata, preddata, survey.area, pred.polys, file="../data/dolphins.RData")
